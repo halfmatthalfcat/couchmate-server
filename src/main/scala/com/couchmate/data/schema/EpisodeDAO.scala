c567b28d2f73d8e9329a010d4ce8971712164890
Matt Oliver <matt@couchmate.com>
2020-01-16 21:59:39 -0500

    Episode DAO

---
+++ src/main/scala/com/couchmate/data/schema/EpisodeDAO.scala
@@ -0,0 +1,56 @@
+package com.couchmate.data.schema
+
+import PgProfile.api._
+import com.couchmate.data.models.Episode
+import slick.lifted.Tag
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class EpisodeDAO(tag: Tag) extends Table[Episode](tag, "episode") {
+  def episodeId: Rep[Long] = column[Long]("episode_id", O.PrimaryKey, O.AutoInc)
+  def seriesId: Rep[Long] = column[Long]("series_id")
+  def season: Rep[Option[Int]] = column[Option[Int]]("season")
+  def episode: Rep[Option[Int]] = column[Option[Int]]("episode")
+  def * = (
+    episodeId.?,
+    seriesId,
+    season,
+    episode,
+  ) <> ((Episode.apply _).tupled, Episode.unapply)
+
+  def seriesFk = foreignKey(
+    "series_episode_fk",
+    seriesId,
+    SeriesDAO.seriesTable,
+  )(
+    _.seriesId,
+    onUpdate = ForeignKeyAction.Cascade,
+    onDelete = ForeignKeyAction.Restrict,
+  )
+}
+
+object EpisodeDAO {
+  val episodeTable = TableQuery[EpisodeDAO]
+
+  def getEpisode(episodeId: Long)(
+    implicit
+    db: Database,
+  ): Future[Option[Episode]] = {
+    db.run(episodeTable.filter(_.episodeId === episodeId).result.headOption)
+  }
+
+  def upsertEpisode(episode: Episode)(
+    implicit
+    db: Database,
+    ec: ExecutionContext,
+  ): Future[Episode] = {
+    episode match {
+      case Episode(None, _, _, _) =>
+        db.run((episodeTable returning episodeTable) += episode)
+      case Episode(Some(episodeId), _, _, _) => for {
+        _ <- db.run(episodeTable.filter(_.episodeId === episodeId).update(episode))
+        e <- db.run(episodeTable.filter(_.episodeId === episodeId).result.head)
+      } yield e
+    }
+  }
+}
