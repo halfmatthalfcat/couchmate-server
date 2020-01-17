e161ea969351aad16682af2fda3b192f510ddc82
Matt Oliver <matt@couchmate.com>
2020-01-16 22:24:53 -0500

    Started SportEventDAO

---
+++ src/main/scala/com/couchmate/data/schema/SportEventDAO.scala
@@ -0,0 +1,44 @@
+package com.couchmate.data.schema
+
+import PgProfile.api._
+import com.couchmate.data.models.SportEvent
+import slick.lifted.Tag
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class SportEventDAO(tag: Tag) extends Table[SportEvent](tag, "sport_event") {
+  def sportEventId: Rep[Long] = column[Long]("sport_event_id", O.PrimaryKey, O.AutoInc)
+  def sportOrganizationId: Rep[Long] = column[Long]("sport_organization_id")
+  def sportEventTitle: Rep[String] = column[String]("sport_event_title")
+  def * = (
+    sportEventId.?,
+    sportOrganizationId,
+    sportEventTitle,
+  ) <> ((SportEvent.apply _).tupled, SportEvent.unapply)
+}
+
+object SportEventDAO {
+  val sportEventTable = TableQuery[SportEventDAO]
+
+  def getSportEvent(sportEventId: Long)(
+    implicit
+    db: Database,
+  ): Future[Option[SportEvent]] = {
+    db.run(sportEventTable.filter(_.sportEventId === sportEventId).result.headOption)
+  }
+
+  def upsertSportEvent(sportEvent: SportEvent)(
+    implicit
+    db: Database,
+    ec: ExecutionContext,
+  ): Future[SportEvent] = {
+    sportEvent match {
+      case SportEvent(None, _, _) =>
+        db.run((sportEventTable returning sportEventTable) += sportEvent)
+      case SportEvent(Some(sportEventId), _, _) => for {
+        _ <- db.run(sportEventTable.filter(_.sportEventId === sportEventId).update(sportEvent))
+        se <- db.run(sportEventTable.filter(_.sportEventId === sportEventId).result.head)
+      } yield se
+    }
+  }
+}
