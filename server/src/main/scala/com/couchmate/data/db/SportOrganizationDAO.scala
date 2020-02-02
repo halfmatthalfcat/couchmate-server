f22e40bae100f65d732da63b6ebb023429401a37
Matt Oliver <mjo2089@gmail.com>
2020-02-01 22:33:04 -0500

    SportOrganizationDAO

---
+++ server/src/main/scala/com/couchmate/data/db/SportOrganizationDAO.scala
@@ -0,0 +1,41 @@
+package com.couchmate.data.db
+
+import com.couchmate.common.models.SportOrganization
+
+class SportOrganizationDAO()(
+  implicit
+  val ctx: CMContext,
+) {
+  import ctx._
+
+  private[this] implicit val sportOrganizationInsertMeta =
+    insertMeta[SportOrganization](_.sportOrganizationId)
+
+  def getSportOrganization(sportOrganizationId: Long) = quote {
+    query[SportOrganization]
+      .filter(_.sportOrganizationId.contains(sportOrganizationId))
+  }
+
+  def getSportOrganizationBySportAndOrg(
+    extSportId: Long,
+    extOrgId: Option[Int],
+  ) = quote {
+    query[SportOrganization]
+      .filter { so =>
+        so.extSportId == extSportId &&
+        so.extOrgId.orNull == extOrgId.orNull
+      }
+  }
+
+  def upsertSportOrganization(sportOrganization: SportOrganization) = quote {
+    query[SportOrganization]
+      .insert(lift(sportOrganization))
+      .onConflictUpdate(_.sportOrganizationId)(
+        (from, to) => from.extSportId -> to.extSportId,
+        (from, to) => from.extOrgId -> to.extOrgId,
+        (from, to) => from.orgName -> to.orgName,
+        (from, to) => from.sportName -> to.sportName,
+      ).returning(so => so)
+  }
+
+}
