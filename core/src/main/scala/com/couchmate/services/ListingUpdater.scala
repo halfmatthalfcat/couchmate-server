1596461ede66219e51bd0fc2215f95c51f03b6c8
Matt Oliver <matt@couchmate.com>
2020-07-12 22:33:10 -0400

    Fix cron schedule

--- core/src/main/scala/com/couchmate/services/ListingUpdater.scala
+++ core/src/main/scala/com/couchmate/services/ListingUpdater.scala
@@ -2,10 +2,12 @@ package com.couchmate.services
 
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.adapter._
 import com.couchmate.common.dao.{ProviderDAO, UserProviderDAO}
 import com.couchmate.common.db.PgProfile.api._
 import com.couchmate.services.gracenote.listing.ListingPullType
 import com.couchmate.util.akka.extensions.{DatabaseExtension, SingletonExtension}
+import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
@@ -24,31 +26,33 @@ object ListingUpdater
   def apply(): Behavior[Command] = Behaviors.setup { ctx =>
     implicit val ec: ExecutionContext = ctx.executionContext
     implicit val db: Database = DatabaseExtension(ctx.system).db
+    val scheduler: QuartzSchedulerExtension = QuartzSchedulerExtension(ctx.system.toClassic)
     val listingCoordinator: ActorRef[ListingCoordinator.Command] =
       SingletonExtension(ctx.system).listingCoordinator
 
-    Behaviors.withTimers { timers =>
-      timers.startTimerWithFixedDelay(StartUpdate, 1 day)
+    scheduler.schedule(
+      "EveryOtherDay",
+      ctx.self.toClassic,
+      StartUpdate,
+      None
+    )
 
-      ctx.self ! StartUpdate
-
-      Behaviors.receiveMessage {
-        case StartUpdate => ctx.pipeToSelf(getProviders) {
-          case Success(value) => ProvidersSuccess(value)
-          case Failure(exception) => ProvidersFailure(exception)
-        }
-          Behaviors.same
-        case ProvidersSuccess(providers) =>
-          providers.foreach { providerId: Long =>
-            ctx.log.debug(s"Starting listing pull for $providerId")
-            listingCoordinator ! ListingCoordinator.RequestListing(
-              providerId,
-              ListingPullType.Initial,
-              ctx.system.ignoreRef
-            )
-          }
-          Behaviors.same
+    Behaviors.receiveMessage {
+      case StartUpdate => ctx.pipeToSelf(getProviders) {
+        case Success(value) => ProvidersSuccess(value)
+        case Failure(exception) => ProvidersFailure(exception)
       }
+        Behaviors.same
+      case ProvidersSuccess(providers) =>
+        providers.foreach { providerId: Long =>
+          ctx.log.debug(s"Starting listing pull for $providerId")
+          listingCoordinator ! ListingCoordinator.RequestListing(
+            providerId,
+            ListingPullType.Full,
+            ctx.system.ignoreRef
+          )
+        }
+        Behaviors.same
     }
   }
 
