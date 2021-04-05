c6c5ec37f5e7e79acd79fdff3c610437280c0e6e
Matt Oliver <matt@couchmate.com>
2021-04-05 16:11:58 -0500

    Remove db spans

--- common/src/main/scala/com/couchmate/common/dao/package.scala
+++ common/src/main/scala/com/couchmate/common/dao/package.scala
@@ -1,8 +1,6 @@
 package com.couchmate.common
 
 import com.couchmate.common.db.PgProfile.api._
-import kamon.Kamon
-import kamon.instrumentation.futures.scala.ScalaFutureInstrumentation.traceBody
 import play.api.libs.json.{Format, JsNull, JsResult, JsValue, Json, JsonConfiguration, OptionHandlers, Writes}
 import scalacache.caffeine.CaffeineCache
 import scalacache.redis.RedisCache
@@ -43,13 +41,9 @@ package object dao {
   ): Future[T] = {
     val fqCacheKey: String =
       DefaultCacheKeyBuilder().toCacheKey(cacheKey)
-    val span = Kamon.currentSpan()
-    span.tag("cacheable", value = true)
-    span.tag("cache-key", fqCacheKey)
-    traceBody(fqCacheKey)(for {
+    for {
       // If we're busting cache, bust it
       _ <- if (bust) {
-        span.tag("force-bust", value = true)
         (for {
           _ <- caffeine.remove(cacheKey: _*)
           _ <- redis.remove(cacheKey: _*)
@@ -58,80 +52,71 @@ package object dao {
           case _: Throwable => Future.successful()
         }
       } else {
-        span.tag("force-bust", value = false)
         Future.successful()
       }
       // First look in-memory for the cache item
-      inMemory <- traceBody("get-caffeine")(get(cacheKey: _*)(
+      inMemory <- get(cacheKey: _*)(
         cache = caffeine,
         mode = implicitly[Mode[Future]],
         flags = Flags()
-      ))
+      )
       // If there isn't anything in memory, continue
-      inMemoryValue <- inMemory.fold({
-        span.tag("in-caffeine", value = false)
+      inMemoryValue <- inMemory.fold(
         Future.successful(Option.empty[T])
         // If there is something in memory, decode it
-      })(value => {
-        span.tag("in-caffeine", value = true)
-        Future.successful(Json.parse(value).asOpt[T])
-      })
+      )(value => Future.successful(Json.parse(value).asOpt[T]))
       // If it doesn't exist in memory, check Redis
       inRedis <- inMemoryValue.fold[Future[Option[T]]](for {
         // Get the item from Redis
-        redisItem <- traceBody("get-redis")(get(cacheKey: _*)(
+        redisItem <- get(cacheKey: _*)(
           cache = redis,
           mode = implicitly[Mode[Future]],
           flags = Flags()
-        )) recover {
+        ) recover {
           case _: Throwable => Option.empty
         }
         // If the item doesn't exist in Redis, continue on
-        redisItemValue <- redisItem.fold({
-          span.tag("in-redis", value = false)
+        redisItemValue <- redisItem.fold(
           Future.successful(Option.empty[T])
           // If there is something in redis, decode it
-        })(value => {
-          span.tag("in-redis", value = true)
-          Future.successful(Json.parse(value).asOpt[T])
-        })
+        )(value => Future.successful(Json.parse(value).asOpt[T]))
         // No decoded value, continue
         inMemItem <- redisItemValue.fold(
           Future.successful(Option.empty[T])
           // If the item does exist in Redis, repopulate the
           // in-memory cache
         )(rItemValue => for {
-          _ <- traceBody("put-caffeine")(put(cacheKey: _*)(
+          _ <- put(cacheKey: _*)(
             Json.toJson(rItemValue).toString,
             ttl.map(_ / ratio.getOrElse(1))
           )(
             cache = caffeine,
             mode = implicitly[Mode[Future]],
             flags = Flags()
-          ))
+          )
         } yield Some(rItemValue))
       } yield inMemItem)(item => Future.successful(Some(item)))
       value <- inRedis.fold(for {
-        resolvedValue <- traceBody("get-value")(fn)
-        _ <- traceBody("put-caffeine")(put(cacheKey: _*)(
+        resolvedValue <- fn
+        _ <- put(cacheKey: _*)(
           Json.toJson(resolvedValue).toString,
           ttl.map(_ / ratio.getOrElse(1))
         )(
           cache = caffeine,
           mode = implicitly[Mode[Future]],
           flags = Flags()
-        ))
-        _ <- traceBody("put-redis")(put(cacheKey: _*)(
+        )
+        _ <- put(cacheKey: _*)(
           Json.toJson(resolvedValue).toString,
           ttl
         )(
           cache = redis,
           mode = implicitly[Mode[Future]],
           flags = Flags()
-        )) recoverWith {
+        ) recoverWith {
           case _: Throwable => Future.successful()
         }
       } yield resolvedValue)(Future.successful)
-    } yield value)
+    } yield value
   }
 }
